f6c6b8cd9f0ed6ed4d8b07fd839a5c49c7a3e9c2
Kevin Morris <kmorris511@yahoo.com>
2012-05-14 19:00:54 -0600

    Initialization; first passing test
    Signed-off-by: Kevin Morris <kmorris511@yahoo.com>

---
+++ pyximity.py
@@ -0,0 +1,6 @@
+from numpy import *
+
+class Pyximity:
+  
+  def __init__(self):
+    pass
---
+++ pyximity_test.py
@@ -0,0 +1,13 @@
+from numpy import *
+import unittest
+from pyximity import *
+
+class PyximityTest(unittest.TestCase):
+  
+  def testRegression(self):
+    
+    p = Pyximity()
+    
+
+if __name__ == '__main__':
+    unittest.main()    
