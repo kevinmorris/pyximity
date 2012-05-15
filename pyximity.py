e80657a517c72c2a8763e7ba939f482a790d6907
Kevin Morris <kmorris511@yahoo.com>
2012-05-15 17:28:50 -0600

    Adding regression method to Pyximity
    Signed-off-by: Kevin Morris <kmorris511@yahoo.com>

--- pyximity.py
+++ pyximity.py
@@ -2,5 +2,20 @@ from numpy import *
 
 class Pyximity:
   
-  def __init__(self):
-    pass
+  def __init__(self, neighbors, values):
+    self.neighbors = neighbors
+    self.values = values
+    
+  def regress(self, target, kernel=lambda dNorm: 1 - dNorm, k=5):
+    neighborCount = self.neighbors.shape[0]
+
+    #Using Euclidian distance
+    distances = ((tile(target, (neighborCount, 1)) - self.neighbors)**2).sum(axis=1)**0.5
+    sortedDistanceIndexes = distances.argsort()[0:k]
+    maxDistance = double(distances[sortedDistanceIndexes[-1]])
+
+    weights = array([double(kernel(distances[i] / maxDistance)) for i in sortedDistanceIndexes])
+    weightSum = sum(weights)
+    weightsNorm = weights / weightSum
+
+    return sum(array([self.values[i] for i in sortedDistanceIndexes]) * weightsNorm)
